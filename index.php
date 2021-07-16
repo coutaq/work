cd4ceb49db5a596f623aa3fcd14acfe7d337f553
coutaq <coutaq@gmail.com>
2021-07-16 13:36:06 +0300

    добавил пользователя в бд

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 namespace work;
 require 'database/DB.php';
 use work\database\DB;
+require 'models/User.php';
+use work\models\User;
 DB::connect();
 if(!DB::$isConnected){
     echo DB::$error;
 }
+
+$user = new User("Test", "test@gmail.com", "phphphp");
+$user->create('users');
 ?>
--- models/User.php
+++ models/User.php
@@ -1,14 +1,28 @@
 <?php
+namespace work\models;
+require 'database/DB.php';
+
+use mysqli;
+use work\database\DB;
 class User{
     public $name;
     public $email;
     public $photo;
 
     public function __construct($name, $email, $photo)
-    {
+    { 
         $this->name = $name;
         $this->email = $email;
         $this->photo = $photo;
+    }
+    public function create($table){
+        echo "создаем";
+        $query = 'INSERT INTO '.$table.' (name, email, photo) VALUES ('.$this->name.','.$this->email.','.$this->photo.')';
+        if(mysqli_query(DB::$connection,$query)){
+            echo "пользователь добавлен";
+        }else{
+            echo mysqli_error(DB::$connection);
+        }
     }
 }
 ?>
