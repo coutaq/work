83c2e82c0272db3f1f8b94d5c31ea0675cd324ee
coutaq <coutaq@gmail.com>
2021-07-20 20:19:17 +0300

    added routing

--- web/index.php
+++ web/index.php
@@ -1,28 +1,31 @@
-<?php 
+<?php// Include router class
+include('web/Route.php');
 
-require(__DIR__.'/../vendor/autoload.php');
-use work\app\auth\auth;
-require './views/header.php';
-if(!Auth::isAuth()){
-    if(isset($_GET['failed'])){
-        $errors = json_decode($_GET['failed']);
-        if(in_array('fields', $errors)){
-            echo '<b>Все поля должны быть заполнены!</b><br/>';
-        }
-        if(in_array('unique', $errors)){
-            echo '<b>Данная почта уже зарегестрирована!</b><br/>';
-        }
-        if(in_array('@', $errors)){
-            echo '<b>Почта должна содержать `@`!</b><br/>';
-        }
-        if(in_array('.', $errors)){
-            echo '<b>Почта должна содержать `.`!</b><br/>';
-        }
-    }
-    require './views/Register.php';
-}else{
-    require './views/users.php';
-}
+// Add base route (startpage)
+Route::add('/',function(){
+    echo 'Welcome :-)';
+});
+
+// Simple test route that simulates static html file
+Route::add('/test.html',function(){
+    echo 'Hello from test.html';
+});
+
+// Post route example
+Route::add('/contact-form',function(){
+    echo '<form method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
+},'get');
+
+// Post route example
+Route::add('/contact-form',function(){
+    echo 'Hey! The form has been sent:<br/>';
+    print_r($_POST);
+},'post');
+
+// Accept only numbers as parameter. Other characters will result in a 404 error
+Route::add('/foo/([0-9]*)/bar',function($var1){
+    echo $var1.' is a great number!';
+});
+
+Route::run('/');
 ?>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
